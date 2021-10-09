30e7170008434a1398c383e02308d11659d27d65
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-10-09 23:27:28 +0300

    Exam completed.

--- day01/wonderful-world/src/main.rs
+++ day01/wonderful-world/src/main.rs
@@ -1,6 +1,29 @@
+use std::f64;
+use std::io::{self};
+
 fn main() {
     println!("Ne güzel bir gün öyle değil mi?");
-    println!("Adım. {}, {}. {} yaşındayım.","Burak","Selim",45);
+    println!("Adım. {}, {}. {} yaşındayım.", "Burak", "Selim", 45);
+    println!(
+        "Argümanların pozisyonlanmasına örnektir. {0} {1} {1} {0}",
+        3, 2
+    );
+    println!(
+        "Argümanlarda isim kullanımına örnektir. {age} {nickname}",
+        age = 45,
+        nickname = "red"
+    );
+    println!(
+        "Sayı formatlamaya örnektir. Pi = {0:.10} veya {0:.0} şeklinde yazılabilir",
+        f64::consts::PI
+    );
+
     print!("Senin adın");
     println!(" ne?");
+    let mut _name = String::new();
+    if let Ok(l) = io::stdin().read_line(&mut _name) {
+        println!("Merhaba {} (Adın {} byte uzunluğunda)", _name, l);
+    } else {
+        eprintln!("Bi sorun var sanki");
+    }
 }
