a03877d84f1790ae311d3ec8e98fae9f71cf1f92
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-10-10 22:04:33 +0300

    Some trait impl added.

--- day01/traits/src/lib.rs
+++ day01/traits/src/lib.rs
@@ -4,16 +4,16 @@ use std::io::{Read, Write};
 ///
 /// # Properties
 ///
-/// values isimli Vector kullanılır. (String,String) ile çalışır.
+/// pairs isimli Vector kullanılır. (String,String) ile çalışır.
 ///
 pub struct Configuration {
-    values: Vec<(String, String)>,
+    pairs: Vec<(String, String)>,
 }
 
 impl Configuration {
     // Configuration struct nesnesi örneklemek için kullanılan fonksiyon
-    pub fn new(_values: Vec<(String, String)>) -> Configuration {
-        Configuration { values: _values }
+    pub fn new(_pairs: Vec<(String, String)>) -> Configuration {
+        Configuration { pairs: _pairs }
     }
 }
 
@@ -30,18 +30,48 @@ impl ConfigurationService {
 }
 
 ///
-/// Belli bir key değerinin karşılığı olan value içeriğini döndüren fonksiyonun temsilidir
+/// Belli bir key değerinin karşılığı olan value içeriğini döndüren bir davranış tanımlar.
 ///
 pub trait ValueGetter {
     fn get(&self, s: &str) -> Option<String>;
 }
 
-pub trait ConfigurationReader {
+///
+/// Konfigurasyonu davranışları
+///
+pub trait ConfigurationBehaviors {
     fn read(&self, from: &mut impl Read) -> std::io::Result<Configuration>;
+    fn write(&self, c: Configuration, to: &mut impl Write) -> std::io::Result<()>;
 }
 
-pub trait ConfigurationWriter {
-    fn write(&self, c: Configuration, to: &mut impl Write) -> std::io::Result<()>;
+// trait implementasyonları
+impl ConfigurationBehaviors for ConfigurationService {
+    fn write(&self, c: Configuration, mut to: &mut impl Write) -> std::io::Result<()> {
+        for pair in c.pairs {
+            writeln!(&mut to, "{0}={1}", pair.0, pair.1)?;
+        }
+        Ok(())
+    }
+
+    fn read(&self, from: &mut impl Read) -> std::io::Result<Configuration> {
+        let mut buffer = String::new();
+        from.read_to_string(&mut buffer)?; // Parametre ile taşınan String içeriği oku
+        let pairs: Vec<(String, String)> = buffer
+            .split_terminator("\n") // Satır bazında ayrıştır
+            .map(|row| row.trim()) // boşlukları kaldır
+            .filter(|row| {
+                // : karakterini de hesaba katarak içeriği filtrele
+                let pos = row.find(":").unwrap_or(0);
+                pos > 0 && pos < row.len() - 1
+            })
+            .map(|row| {
+                // süzülen içeriği Tuple'a dönüştür
+                let parts = row.split("=").collect::<Vec<&str>>();
+                (parts[0].to_string(), parts[1].to_string())
+            })
+            .collect(); // Map edilen içeriği Vector nesnesine al
+        Ok(Configuration::new(pairs)) // Ayarları yeni Configuration nesnesi olarak döndür
+    }
 }
 
 #[cfg(test)]
