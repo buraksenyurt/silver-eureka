a3c9111f09743b332c36a86e01812a4e45f8f7e7
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-10-15 13:57:56 +0300

    push and pop fn added.

--- day02/linked_list/src/lib.rs
+++ day02/linked_list/src/lib.rs
@@ -24,8 +24,8 @@ struct Node {
 
 // Linked List veri yapısı
 struct ProcessLog {
-    first: Link, // İlk boğum referansı
-    last: Link, // Son boğum referansı
+    first: Link,    // İlk boğum referansı
+    last: Link,     // Son boğum referansı
     pub count: u64, // Toplam eleman sayısı
 }
 
@@ -37,5 +37,51 @@ impl Node {
             info: info,
             next: None,
         }))
+    }
+}
+
+impl ProcessLog {
+    // Yeni bir ProcessLog nesnesi üretir
+    pub fn new() -> ProcessLog {
+        ProcessLog {
+            first: None, // Option enum sabiti gereği başlangıçta None olarak set edilir
+            last: None,
+            count: 0,
+        }
+    }
+
+    // Listeye yeni bir Node eklemek için kullanılır
+    pub fn push(&mut self, info: String) {
+        let new_node = Node::new(info); // gelen veri ile yeni bir Node nesnesi örneklenir
+
+        match self.last.take() {
+            // pattern matchin ile liste sonundaki Node örneğine bakılır
+            Some(there) => there.borrow_mut().next = Some(new_node.clone()), // Eğer sonda bir eleman varsa onun sonraki referans adresine new_node'un bir klonu eklenir
+            None => self.first = Some(new_node.clone()), // Sonda bir elaman yoksa liste boş anlamına gelir ve new_node'un klonu ilk eleman olarak yerleştirilir
+        };
+
+        self.count += 1; // eleman sayısı bir artırırlır
+        self.last = Some(new_node); // Match ne olursa olsun son eleman yeni üretilen eleman olacaktır
+    }
+
+    // Son eklenen elemanı listeden çekmek(aynı zamanda kaldırmak) için kullanılır
+    pub fn pop(&mut self) -> Option<String> {
+        self.first.take().map(|item| {
+            // ilk elemandan itibaren map çağrısı ile devam eden Node örneklerine bakılan bir iterasyon başlatılır. Bu iterasyon sondaki elemanı hariç tutarak ProcessLog'un first ve last konumlandırmalarını değiştirmek üzere işletilir. Tipik Pop işlevi.
+            if let Some(next) = item.borrow_mut().next.take() { // Eğer takip eden Node değeri varsa
+                self.first = Some(next); // Onu first değişkenine alır. İterasyon son elemana gelene kadar first değişmeye devam eder
+            } else { // Artık arkadan gelen bir Node kalmamışsa
+                self.last.take();  // ProcessLog'un son elemanı olarak kabul edilir
+            }
+
+            self.count -= 1; // Eleman sayısı bir azaltılır
+
+            // iterasyonun son elemanının değeri alınır 
+            Rc::try_unwrap(item) 
+                .ok()
+                .expect("Bir sorun gördüm sanki. Gördüm, gördüm. Bi sorun gördüm.")
+                .into_inner()
+                .info
+        })
     }
 }
