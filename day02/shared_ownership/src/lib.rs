495f89f55c1cd9f4e8b8c57846f6ae4326e185f4
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-10-17 23:21:43 +0300

    rc bench added.

--- day02/shared_ownership/src/lib.rs
+++ day02/shared_ownership/src/lib.rs
@@ -2,18 +2,27 @@
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use std::rc::Rc;
     extern crate test;
     use test::{black_box, Bencher};
+
+    #[bench] // #4
+    fn bench_test_for_reference_counting(runner: &mut Bencher) {
+        let word: String = (0..100_000_000).map(|_| 'r').collect();
+        let tenant = Rc::new(word); // word için yeni bir smart pointer oluşturulur. word değerini değil pointer lokasyonunu sahiplenir.
+        runner.iter(|| {
+            black_box(get_length_with_rc(tenant.clone())); // clone fonksiyonunun çağırıldığı bir black_box test çalışıtırılır.
+        });
+    }
 
     #[bench] // #3
     fn bench_test_for_clone(runner: &mut Bencher) {
         // clone çağrımı maliyetlidir çünkü string'in birçok kopyası oluşturulur.
         // Bu durumu analiz etmek için rust'ın nightly build'unu kullanıp benchmark modülünden yararlanıyoruz.
         // Terminalden cargo bench şeklinde çalıştırılır.
-        
+
         // Test için büyük bir string oluşturulur
-        let word: String = (0..100_000_000).map(|_| 'r').collect(); 
+        let word: String = (0..100_000_000).map(|_| 'r').collect();
         runner.iter(|| {
             black_box(get_length(word.clone())); // clone fonksiyonunun çağırıldığı bir black_box test çalışıtırılır.
         });
@@ -34,8 +43,15 @@ mod tests {
         assert_eq!(get_length(word), 9); // Dikkat!
                                          // assert_eq!(word.len(), 9); // word değişkeni get_length fonksiyonuna aktarıldıktan sonra scope dışında kalır ve bu nedenle bu satırda derleyici hata verir. Açıp hatayı görün
     }
-}
 
-pub fn get_length(something: String) -> usize {
-    something.len()
+    fn get_length(something: String) -> usize {
+        something.len()
+    }
+
+    // Fonksiyonda gelen String için sahipliği Reference Counted olarak akıllı bir işaretçiye (smart pointer) devretmekteyiz.
+    // Bu durumda bir String için sürekli deep copy yapmak yerine pointer'ın çoğaltılması söz konusudur. Bu performans açısından iyi sonuç verir.
+    // #4 numaralı bench testinde kullanılmakta
+    fn get_length_with_rc(something: Rc<String>) -> usize {
+        something.len()
+    }
 }
