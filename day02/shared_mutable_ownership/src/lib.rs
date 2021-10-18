7f044dbf703c9c532b55f3b98fb8c115d87f54e6
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-10-18 23:06:13 +0300

    some bench fn added.

--- day02/shared_mutable_ownership/src/lib.rs
+++ day02/shared_mutable_ownership/src/lib.rs
@@ -9,12 +9,23 @@ mod tests {
     use std::cell::{Cell, RefCell};
 
     #[bench] // #1 Klasik yöntemle vector içeriğini değiştirmek
-    fn regular_mutation_test(runner: &mut Bencher) {
+    fn mutation_with_regular_bench(runner: &mut Bencher) {
         // mutable tipte bir vector verisine çok sayıda 1 ekleniyor.
         let mut data = vec![];
         runner.iter(|| {
             for _ in 0..1_000_000 {
                 data.push(1);
+            }
+        });
+    }
+
+    #[bench] // #2 RefCell ile vector içeriğini değiştirmek
+    fn mutation_with_refcell_bench(runner: &mut Bencher) {
+        // vector için bir RefCell referansı oluşturuyoruz
+        let data_ref = RefCell::new(vec![]);
+        runner.iter(|| {
+            for _ in 0..1_000_000 {
+                data_ref.borrow_mut().push(1); //borrow_mut ile RecCell'in sahiplendiği vecktörü ödünç alıyor ve 1 ekliyoruz
             }
         });
     }
