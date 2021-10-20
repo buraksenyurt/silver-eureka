1eae41eb7a99449bdab42cbd0ede46968e32b2ea
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-10-20 21:13:54 +0300

    new case added.

--- day02/lifetimes/src/lib.rs
+++ day02/lifetimes/src/lib.rs
@@ -53,7 +53,8 @@ mod tests {
 
     // lifetime bildirimleri 'a gibi yapılır. a yerine başka bir şeyde yazılabilir ama genelde 'a 'b gibi tercih ediliyor. .Net tarafındaki generic T,K gibi düşünebiliriz.
     // lifetime bildirimleri fonksiyonun imzasına konan ek bildirimlerdir ve Rust derleyicisi için bilgi sağlar.
-    // 'a parametre olarak gelen referanslardan en küçük yaşam süresi olana göre hareket eder ve dönüş referansı için de aynı yaşam süresini kullanır.
+    // 'a bildirimi parametre olarak gelen referanslardan en küçük yaşam süresi olana göre hareket eder(ki örnekte word_1 ve word_2 aynı scope'ta olduklarından yaşam süreleri de aynıdır)
+    // ve dönüş referansı için de aynı yaşam süresini kullanır.
     fn who_wins_with_lt<'a>(my_word: &'a str, your_word: &'a str) -> &'a str {
         if your_word.len() > my_word.len() {
             // ve hangisinin uzunluğu büyükse o referansı fonksiyonun çağırıldığı yere döndürür. Gayet masumane değil mi?
@@ -61,5 +62,33 @@ mod tests {
         } else {
             my_word
         }
+    }
+
+    #[test] // #4 Yaşam süreleri var oldukları scope'lar gereği farklı olan değişkenler bu fonksiyonla kullanılmak istenirse.
+            // Aşağıdaki senaryoda hiçbir problem yaşamayız ve borrow cheker bunu kabul eder. word_2 ile winner aynı scope yaşam ömrüne sahiptirler. word_1 ise daha uzun ömürlüdür.
+            // yine de sonucu tutacak olan referansın yaşam döngüsü a' bildirimine göre borrow cheker tarafında kabul görür.
+            // Ama #5 senaryosuna gidersek...
+    fn different_lifetimes_test() {
+        let word_1 =
+            "Bir berber bir berbere gel bereber berberistanda bir berber dükkanı açalım demiş";
+        {
+            let word_2="Şu yoğurdu nane ile birlikte sarımsaklasak da mı saklasak nane ile sarımsaklamasak da mı saklamasak. Nanesiz sarımsaklasak da mı saklamasak?";
+            let winner = who_wins_with_lt(word_1, word_2);
+            assert_eq!(winner.len(), 148);
+        }
+    }
+
+    #[test] // #5 Winner değişkenini fonksiyon çağrısının yapıldığı iç scope dışına aldık.
+            // ama daha da önemlisi word_2 değişkeninin iç scope dışında geçersiz hale gelmesine neden olacak şekilde from ile oluşturup as_str ile taşıdık.
+            // scope dışında word_2 nin ömrü sonlanıyor. Borrow cheker ise 'a bildirimi nedeniyle hem parametre hem de dönüş referansının aynı yaşam ömrüne sahip olmasını bekliyor.
+    fn different_lifetimes_violation_test() {
+        let word_1 =
+            "Bir berber bir berbere gel bereber berberistanda bir berber dükkanı açalım demiş";
+        let winner;
+        {
+            let word_2=String::from("Şu yoğurdu nane ile birlikte sarımsaklasak da mı saklasak nane ile sarımsaklamasak da mı saklamasak. Nanesiz sarımsaklasak da mı saklamasak?");
+            winner = who_wins_with_lt(word_1, word_2.as_str());
+        }
+        assert_eq!(winner.len(), 148);
     }
 }
