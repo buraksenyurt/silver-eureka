cf9d81e2c32e3fce500115cf7b070e7b39068e63
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-10-20 20:23:21 +0300

    add borrow cheker test

--- day02/lifetimes/src/lib.rs
+++ day02/lifetimes/src/lib.rs
@@ -4,15 +4,27 @@
 
 #[cfg(test)]
 mod tests {
-    #[test] // #1 İlk durum
-    fn borrow_cheker_on_play_test() {
-        let tenant; // reference isimli bir değişken tanımlanır. Yaşam ömrü burada başlıyor. Bunu 'a ile gösterelim.
+    // #[test] // #1 İlk durum
+    // fn borrow_cheker_on_play_test() {
+    //     let tenant; // reference isimli bir değişken tanımlanır. Yaşam ömrü burada başlıyor. Bunu 'a ile gösterelim.
+    //     {
+    //         // iç alanda point isimli bir değişken tanımlı. Bunun ömrü sadece bu blok için geçerli. Bunu da 'b ile gösterelim.
+    //         let point = 10;
+    //         tenant = &point; // point değişkenin bellek referansını tenant isimli değişkene aldık.
+    //     } // iç scope'un sonlandığı yer. point değişkeninin ömrü burada bitti. 'b için son. Lakin onun referansını tenant değişkenine almıştık. tenant'a sahiplik verdik dolayısıyla.
+    //     assert_eq!(tenant, &10); // artık bu noktada tenant'ın referans ettiği point var olmadığı için tenant'ın bellek mevkisi kaldırılmış bir yeri işaret etme riski söz konusudur.
+    //                             // Rust'ın borrow_checker mekanizması bu kullanıma müsaade etmez ve derleme hatası oluşur.
+    // } // Burası 'a için yaşam döngüsünün sonlandığı yer.
+
+
+    #[test] // #2 Birdeki durumun çözümü
+    fn valid_borrow_cheker_test() {
         {
-            // iç alanda point isimli bir değişken tanımlı. Bunun ömrü sadece bu blok için geçerli. Bunu da 'b ile gösterelim.
+            // durumu göstermek için bir scope açıldı. tenant ve point değişkenleri aynı scope içerisindeler. yaşam döngüleri borrow cheker açısından bu scope dahilinde kullanıma müsait.
+            let tenant;
             let point = 10;
-            tenant = &point; // point değişkenin bellek referansını tenant isimli değişkene aldık.
-        } // iç scope'un sonlandığı yer. point değişkeninin ömrü burada bitti. 'b için son. Lakin onun referansını tenant değişkenine almıştık. tenant'a sahiplik verdik dolayısıyla.
-        assert_eq!(tenant, &5); // artık bu noktada tenant'ın referans ettiği point var olmadığı için tenant'ın bellek mevkisi kaldırılmış bir yeri işaret etme riski söz konusudur.
-                                // Rust'ın borrow_checker mekanizması bu kullanıma müsaade etmez ve derleme hatası oluşur.
-    } // Burası 'a için yaşam döngüsünün sonlandığı yer.
+            tenant = &point;
+            assert_eq!(tenant, &10);
+        }
+    }
 }
