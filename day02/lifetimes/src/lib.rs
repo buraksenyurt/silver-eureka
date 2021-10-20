0e184ff5b168f50ca4dfdf3b1a2adffe8ebfb366
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-10-20 20:42:40 +0300

    before generic lifetimes.

--- day02/lifetimes/src/lib.rs
+++ day02/lifetimes/src/lib.rs
@@ -16,7 +16,6 @@ mod tests {
     //                             // Rust'ın borrow_checker mekanizması bu kullanıma müsaade etmez ve derleme hatası oluşur.
     // } // Burası 'a için yaşam döngüsünün sonlandığı yer.
 
-
     #[test] // #2 Birdeki durumun çözümü
     fn valid_borrow_cheker_test() {
         {
@@ -25,6 +24,29 @@ mod tests {
             let point = 10;
             tenant = &point;
             assert_eq!(tenant, &10);
+        }
+    }
+
+    #[test] // #3 Bu senaryoda generic lifetime senaryosu öncesi problemin ne olduğuna bakılıyor
+    // Hata alınması Rust derleyici açısında normal ve gereklidir.
+    // who_wins fonksiyonuna iki string referansı gider ama hangisinin döneceğini bilemez. (koşula giren string uzunluklarının dinamik geldiğini düşünecek olursak)
+    // Ayrıca borrow cheker mekanizması da bu durumu analiz edemez nitekim gelen referanslar ile dönen referansın yaşam ömürlerin arasında ilişkiyi bilemez.
+    // Bu sebeple who_wins fonksiyonu değiştirilmeli ve lifetime için dipnot bırakılmaı bir başka deyişler fonksiyon parametre ve dönüş referansları için borrow cheker'ın bu kontrolü atlamasını sağlamalıyız.
+    fn need_for_generic_lifetimes_test() {
+        let word_1 =
+            "Bir berber bir berbere gel bereber berberistanda bir berber dükkanı açalım demiş";
+        let word_2="Şu yoğurdu nane ile birlikte sarımsaklasak da mı saklasak nane ile sarımsaklamasak da mı saklamasak. Nanesiz sarımsaklasak da mı saklamasak?";
+        let winner = who_wins(word_1, word_2);
+        assert_eq!(winner.len(), 140);
+    }
+
+    // who_wins fonksiyon string değişkenler için referanslarını alır ki bu son derece doğaldır nitekim büyük String içeriklerin sahipliğini onları yolladığımız fonksiyonun alması önerilmez.
+    fn who_wins(my_word: &str, your_word: &str) -> &str {
+        if your_word.len() > my_word.len() {
+            // ve hangisinin uzunluğu büyükse o referansı fonksiyonun çağırıldığı yere döndürür. Gayet masumane değil mi?
+            your_word
+        } else {
+            my_word
         }
     }
 }
