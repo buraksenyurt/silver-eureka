d0aa3e150bbff3c6337bba48a7054be0bfb310ce
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-10-25 15:11:21 +0300

    another trait code added.

--- day02/trait_bounds/src/lib.rs
+++ day02/trait_bounds/src/lib.rs
@@ -1,3 +1,5 @@
+use std::fmt::Debug;
+
 // trait bounds sözdiziminin kullanılmasına dair bir örnek
 
 #[cfg(test)]
@@ -41,17 +43,49 @@ mod tests {
         assert_eq!(result[0],"Monolitik Sistemlerde Teknik Borçlanma ile Mücadele -> Burak Selim Şenyurt -> Yazılımcı olmanın bir gerçeği de üretim ...");
         assert_eq!(result[1], "Boba the Fat -> The Empire Strikes Back");
     }
+
+    #[test]
+    fn should_debug_trait_works_test() {
+        let notify = Notification {
+            owner: String::from("Boba the Fat"),
+            note: String::from("The Empire Strikes Back"),
+            is_argent: true,
+        };
+        // #3
+        // log fonksiyonuna notification veri yapısı türünden bir değişken gönderebilmemiz için Debug trait'inin uygulanması gerekir
+        assert_eq!(log(notify), "Notification { owner: \"Boba the Fat\", note: \"The Empire Strikes Back\", is_argent: true }");
+    }
 }
 
-// #1
-// Şimdi Summary trait'ini parametre olarak alıp kullanan bir fonksiyon olduğunu düşünelim.
-// item_a ve item_b Summary trait'ini implemente eden veri yapıları olmalıdır
-pub fn collect_summaries(item_a: impl Summary, item_b: impl Summary) -> Vec<String> {
+// #3
+// Şimdi aşağıdaki Trait'i göz önüne alalım
+// T tipinin Rust sisteminde önceden tanımlı olan Debug davranışını sergileyebilir olması bekleniyor
+pub fn log<T: Debug>(item: T) -> String {
+    format!("{:?}", item)
+}
+
+// #2 Üstteki fonksiyonu trait bounds formatında ve where koşulunu kullanarak aşağıdaki gibi yazabiliriz.
+// item_a ve item_b farklı türden veri yapıları olsun ama her ikisi de Summary davranışını uygulamış olsun istiyoruz
+pub fn collect_summaries<T, K>(item_a: T, item_b: K) -> Vec<String>
+where
+    T: Summary,
+    K: Summary,
+{
     let mut result: Vec<String> = vec![];
     result.push(item_a.get_summary());
     result.push(item_b.get_summary());
     result
 }
+
+// // #1
+// // Şimdi Summary trait'ini parametre olarak alıp kullanan bir fonksiyon olduğunu düşünelim.
+// // item_a ve item_b Summary trait'ini implemente eden veri yapıları olmalıdır
+// pub fn collect_summaries(item_a: impl Summary, item_b: impl Summary) -> Vec<String> {
+//     let mut result: Vec<String> = vec![];
+//     result.push(item_a.get_summary());
+//     result.push(item_b.get_summary());
+//     result
+// }
 
 // Önce kendi trait tipimizi oluşturalım
 // Herhangi bir tip içindeki bilgileri kullanarak özet bir ifade veren davranış tanımı için kullanacağımızı düşünelim
@@ -67,6 +101,7 @@ pub struct BlogEntry {
 }
 
 // Bir başka örnek veri modeli (Bir duyuru metnini temsil etsin)
+#[derive(Debug)] // #3 kullanımının çalışması için eklenmesi gereken Trait bildirimi
 pub struct Notification {
     pub owner: String,
     pub note: String,
