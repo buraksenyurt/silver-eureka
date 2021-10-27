c940312c6bc6fb6a8d3e9d6245206550664ffe73
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-10-27 16:48:48 +0300

    some lib added.

--- day03/rogue-one/business/src/lib.rs
+++ day03/rogue-one/business/src/lib.rs
@@ -1,8 +1,37 @@
+// Workspace içine açtığımız sembolik bir modül.
+// Player veri yapısı için iki fonksiyon ve test içerir
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn should_increase_player_works() {
+        let mut dragon = Player::new("dragon".to_string(), 56);
+        assert_eq!(dragon.to_str(), "dragon(56)");
+        dragon.increase_level(4);
+        assert_eq!(dragon.level, 60);
+    }
+}
+
+#[derive(Debug)]
+pub struct Player {
+    nickname: String,
+    level: i32,
+}
+impl Player {
+    pub fn new(n: String, l: i32) -> Player {
+        Player {
+            nickname: n,
+            level: l,
+        }
+    }
+
+    pub fn increase_level(&mut self, point: i32) -> &Player {
+        self.level += point;
+        self
+    }
+
+    pub fn to_str(&self) -> String {
+        format!("{}({})", self.nickname, self.level)
     }
 }
