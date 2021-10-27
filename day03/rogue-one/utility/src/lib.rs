96e049453f2cbf402e2f33696a17723346ddc6a0
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-10-27 17:04:45 +0300

    another dummy lib added.

--- day03/rogue-one/utility/src/lib.rs
+++ day03/rogue-one/utility/src/lib.rs
@@ -1,8 +1,22 @@
+// workspace içinde kullanılan bir başka sembolik crate
+// sadece can_play isimli bir fonksiyon ve testini içeriyor
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn should_can_play_works_test() {
+        assert_eq!(can_play(79), Some(0));
+        assert_eq!(can_play(80), Some(1));
+        assert_eq!(can_play(0), Some(0));
+        assert_eq!(can_play(180), None);
+    }
+}
+
+pub fn can_play(level_point: u32) -> Option<u8> {
+    match level_point {
+        0..=79 => Some(0),
+        80..=100 => Some(1),
+        _ => None,
     }
 }
