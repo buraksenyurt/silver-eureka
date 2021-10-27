bfee185810381e179025ec93533fdb134c1be2f2
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-10-27 22:14:43 +0300

    dummy queue struct added.

--- day03/event-queue/src/lib.rs
+++ day03/event-queue/src/lib.rs
@@ -16,19 +16,33 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_new_event() {
+    fn should_create_an_event_works_test() {
         let t = SystemTime::now();
         let e = Event::new("Product_Added".to_string(), t);
         assert_eq!(e.to_str(), format!("Product_Added [{:?}]", t));
+    }
+
+    #[test]
+    fn should_create_an_eventqueue_works_test() {
+        let mut event_queue = EventQueue::new();
+        assert_eq!(event_queue.count(), 0);
+
+        let event_1 = Event::new("Product_Created".to_string(), SystemTime::now());
+        event_queue.enqueue(event_1);
+        let event_2 = Event::new("Basket_Refreshed".to_string(), SystemTime::now());
+        event_queue.enqueue(event_2);
+        let event_3 = Event::new("Product_Added".to_string(), SystemTime::now());
+        event_queue.enqueue(event_3);
+        assert_eq!(event_queue.count(), 3);
     }
 }
 
 use std::time::SystemTime;
 
 /// Bir olayı sembolize eden veri yapısıdır.
-/// 
+///
 /// # Arguments
-/// 
+///
 /// * `name` - Olayın adı.
 /// * `time` - Olayın zamanı.
 pub struct Event {
@@ -39,12 +53,51 @@ pub struct Event {
 /// Event veri türü ile ilgili fonksiyonlar.
 impl Event {
     /// Yeni bir event değişkeni oluşturur.
-    pub fn new(n: String, t: SystemTime) -> Event {
+    pub fn new(n: String, t: SystemTime) -> Self {
         Event { name: n, time: t }
     }
 
     // Event içeriğini String formatta dönen fonksiyondur
     pub fn to_str(&self) -> String {
         format!("{} [{:?}]", self.name, self.time)
+    }
+}
+
+///
+/// Bir olaylar dizisi kuyruğunu temsil eden veri yapısıdır.
+/// Queue mantığında çalışır.
+/// FIFI - First In First Out
+pub struct EventQueue {
+    pub events: Vec<Event>,
+}
+
+impl EventQueue {
+    /// Yeni bir EventQueue nesnesi oluşturmak için kullanılır
+    pub fn new() -> Self {
+        EventQueue { events: Vec::new() }
+    }
+
+    /// Kuyruğa Event eklemek için
+    pub fn enqueue(&mut self, e: Event) {
+        self.events.push(e)
+    }
+
+    /// İlk eklenen elemanı kuyruktan çıkarır ve geriye döndürür
+    pub fn dequeue(&mut self) -> Event {
+        self.events.remove(0)
+    }
+
+    /// Kuyruktaki eleman sayısını elde etmek için
+    pub fn count(&self) -> usize {
+        self.events.len()
+    }
+
+    /// İlk eklenen elemanı verir ama kuyruktan çıkarmaz
+    pub fn peek(&self) -> Option<&Event> {
+        let e = self.events.first();
+        match e {
+            Some(_) => e,
+            None => None,
+        }
     }
 }
