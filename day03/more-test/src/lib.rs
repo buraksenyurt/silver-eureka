ae705c09da22c4cbb8c8728e5041fb86c19cc355
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-10-30 15:19:43 +0300

    some changes

--- day03/more-test/src/lib.rs
+++ day03/more-test/src/lib.rs
@@ -12,13 +12,31 @@ mod tests {
     }
 
     #[test]
-    fn wait_a_few_seconds_test() {
-        sleep(Duration::from_secs(5)); // Thread şu andan itibaren 5 saniye boyunca durdurulacak
-        println!("Bir süre sonra..."); // Normalde terminalde yapılan cargo test işlemi sonrası println! fonksiyonları görünmez.
+    fn wait_3_seconds_test() {
+        sleep(Duration::from_secs(3)); // Thread şu andan itibaren 3 saniye boyunca durdurulacak
+        println!("3 saniye kadar sonra..."); // Normalde terminalde yapılan cargo test işlemi sonrası println! fonksiyonları görünmez.
         assert_eq!(sum(1, 2), 3);
+    }
+
+    #[test]
+    fn wait_5_seconds_test() {
+        sleep(Duration::from_secs(5)); // Thread şu andan itibaren 5 saniye boyunca durdurulacak
+        println!("5 saniye kadar sonra...");
+        assert_eq!(sum(1, 2), 3);
+    }
+
+    #[test]
+    #[ignore] // ilgili test çalıştırılmaz
+    fn out_of_test() {
+        let conn = get_connection();
+        assert_eq!(conn, "data source=localhost;database=Nortwhind");
     }
 }
 
 pub fn sum(x: i32, y: i32) -> i32 {
     x + y
+}
+
+pub fn get_connection() -> String {
+    "data source=localhost;database=Nortwhind".to_string()
 }
