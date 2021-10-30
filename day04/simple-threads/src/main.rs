8f62b88f27abb06489b083cbcb06ea6215edf8f7
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-10-30 22:54:42 +0300

    Some fixes

--- day04/simple-threads/src/main.rs
+++ day04/simple-threads/src/main.rs
@@ -1,13 +1,18 @@
 use std::thread;
-use std::time::duration;
+use std::time::Duration;
 
 fn main() {
+    // do_something fonksiyonunda bir thread başlatılmakta
     let thread_1 = do_something();
 
+    // o sırada main bir takım işlere başlıyor
     for _ in 0..10 {
         println!(".");
     }
 
+    // bu noktaya gelindiğinde main thread, thread_1 sonlanıncaya kadar bekliyor
+    // nitakim join çağrısı söz konusu.
+    // Eğer join ile diğer thread'in sonlanmasını bekletmezsek main akışı hemen sonlanacaktır.
     println!(
         "Başlatılan thread'in sonlanması için bekleniyor. {:?}",
         thread_1.join()
@@ -18,7 +23,7 @@ fn main() {
 fn do_something() -> thread::JoinHandle<()> {
     thread::spawn(|| {
         println!("Bir takım işler...");
-        thread::sleep(Duration::from_sec(5)); // Uzun bir işi simüle etmek için...
-        prinln!("İşler tamamlandı.");
+        thread::sleep(Duration::from_secs(5)); // Uzun bir işi simüle etmek için...
+        println!("İşler tamamlandı.");
     })
 }
