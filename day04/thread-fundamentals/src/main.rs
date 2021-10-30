54f260b2e78058167a46ff72359169740f738833
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-10-31 00:17:51 +0300

    join usage added.

--- day04/thread-fundamentals/src/main.rs
+++ day04/thread-fundamentals/src/main.rs
@@ -3,17 +3,38 @@ use std::time::Duration;
 
 // İlk çalışmada ana thread içinden yeni bir thread başlatılıyor
 fn main() {
-    // bir thread başlatmak için spawn fonksiyonu kullanılabilir
-    thread::spawn(|| {
-        for i in 1..10 {
-            println!("Spwaned\t\t{}", i);
-            thread::sleep(Duration::from_millis(200)); //sembolik bir duraksatma (200 milisaniye)
-        }
-    });
+    //case_1();
+    let t1 = case_2(); // case_2 fonksiyonu JoinHandle döndürür.
 
     // ana thread içinde çalışan bir döngü
     for i in 1..5 {
         println!("Main\t\t{}", i);
         thread::sleep(Duration::from_millis(100)); // sembolik bir duraksatma (100 milisaniye)
     }
+    t1.join().unwrap(); // Ana thread'i t1'in işaret ettiği thread işleyişini bitirene kadar bekletecektir.
 }
+
+fn case_2() -> thread::JoinHandle<()> {
+    thread::spawn(|| {
+        for i in 1..10 {
+            println!("Spwaned\t\t{}", i);
+            thread::sleep(Duration::from_millis(200)); //sembolik bir duraksatma (200 milisaniye)
+        }
+    })
+}
+
+// fn case_1() {
+//     // bir thread başlatmak için spawn fonksiyonu kullanılabilir
+//     thread::spawn(|| {
+//         for i in 1..10 {
+//             println!("Spwaned\t\t{}", i);
+//             thread::sleep(Duration::from_millis(200)); //sembolik bir duraksatma (200 milisaniye)
+//         }
+//     });
+
+//     // ana thread içinde çalışan bir döngü
+//     for i in 1..5 {
+//         println!("Main\t\t{}", i);
+//         thread::sleep(Duration::from_millis(100)); // sembolik bir duraksatma (100 milisaniye)
+//     }
+// }
