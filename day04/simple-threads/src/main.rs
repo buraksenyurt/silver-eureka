3b7f7dab94dd8c54e222b94a71fa8c11a8fddb6c
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-10-30 23:12:23 +0300

    second case added

--- day04/simple-threads/src/main.rs
+++ day04/simple-threads/src/main.rs
@@ -2,10 +2,12 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
+
+    // #1
     // do_something fonksiyonunda bir thread başlatılmakta
     let thread_1 = do_something();
 
-    // o sırada main bir takım işlere başlıyor
+    // O sırada main bir takım işlere başlıyor
     for _ in 0..10 {
         println!(".");
     }
@@ -17,13 +19,41 @@ fn main() {
         "Başlatılan thread'in sonlanması için bekleniyor. {:?}",
         thread_1.join()
     );
+    // #1
+
+    // #2 
+    // Bir diğer thread'in başlatılacağı kısım.
+    let n = 1;
+    let numbers = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let thread_2 = do_something_with(n, numbers); // parametreleri ile fonksiyon çağrısı
+    println!("Artım değeri {}", n); // do_something_with fonksiyonuna parametre olarak gönderip orada kullandığımız n değişkenine main thread içinden erişmeye çalıştığımız yer. move kullanmadığımızda hata alınır.
+    println!(
+        "Diğer işin tamamlanmasını bekliyoruz. {:?}",
+        thread_2.join()
+    );
+    // #2
 }
 
+// #1
 // Fonksiyon bir thread başlatmakta.
 fn do_something() -> thread::JoinHandle<()> {
     thread::spawn(|| {
         println!("Bir takım işler...");
         thread::sleep(Duration::from_secs(5)); // Uzun bir işi simüle etmek için...
         println!("İşler tamamlandı.");
+    })
+}
+
+// #2
+// Yine içinde thread başlatılan ama metoda gelen parametreleri de kullanan bir fonksiyon söz konusu
+fn do_something_with(increase: i32, values: Vec<i32>) -> thread::JoinHandle<Vec<i32>> {
+    thread::spawn(|| {
+        for i in values.iter() {
+            // values vektöründe gelen sayıları yanyana basıyoruz
+            print!(" {} ", i);
+        }
+        println!("");
+        println!("Buradaki sayı {}", increase);
+        values // sahipliği(ownersghip) geri döndürdüğümüz nokta
     })
 }
