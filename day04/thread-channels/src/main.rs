795384814803f37c75f9f2cdab906335e3971215
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-11-02 21:59:14 +0300

    sample completed.

--- day04/thread-channels/src/main.rs
+++ day04/thread-channels/src/main.rs
@@ -1,6 +1,7 @@
 // Örnekte kanallar yardımıyla thread'ler arası mesaj bazlı haberleşme konusu ele alınmakta.
 // Burada Multi Producer Single Consumer olarak da ifade edebileceğimiz mpsc modülü kullanılmakta.
-// FIFO (First In First Out) ilkesine göre çalışan bir yapı.
+// FIFO (First In First Out) ilkesine göre çalışan bir yapıdır.
+// Çok sayıda mesaj göndericisi ve tek bir alıcı olan senaryolarda kullanılır.
 // İlk kullanımda Simple Streaming söz konusu.
 use rand::prelude::*;
 use std::sync::mpsc;
@@ -13,31 +14,57 @@ fn main() {
     // Bir gönderici ve bir alıcı mevcuttur.
     let (s, r): (Sender<Symbol>, Receiver<Symbol>) = mpsc::channel();
 
-    let smile_sender = s.clone(); // bir sender kopyası aldık
+    let pingpong_sender = s.clone(); // bir sender kopyası aldık
 
     // thread başlatıyoruz
     // içinde sonsuz bir döngü söz konusu
     thread::spawn(move || loop {
         thread::sleep(Duration::from_secs(1));
         // send metodu ile Symbol tipinden bir enum sabitini kanala yolluyoruz.
-        smile_sender
-            .send(Symbol::Smile(random::<usize>() % 10))
+        pingpong_sender
+            .send(Symbol::PingPong(random::<usize>() % 10)) //rastgele sayının 10lu modunu enum sabitine parametre olarak veriyoruz. Kaç tane sembol üreteceğimizi belirtecek.
             .unwrap();
     });
 
     // yeni bir sender kopyası aldık
-    let hearth_sender = s.clone();
+    let bowling_sender = s.clone();
     // içinde sonsuz döngü barındıran yeni bir thread açıyoruz
     thread::spawn(move || loop {
         thread::sleep(Duration::from_secs(1));
-        // hearth_sender bu kez Heart türünden bir enum sabitini kanala gönderiyor
-        hearth_sender
-            .send(Symbol::Hearth(random::<usize>() % 10))
+        // bowling_sender bu kez Bowling türünden bir enum sabitini kanala gönderiyor
+        bowling_sender
+            .send(Symbol::Bowling(random::<usize>() % 10))
             .unwrap();
     });
+
+    let rugby_sender = s.clone();
+    thread::spawn(move || loop {
+        thread::sleep(Duration::from_secs(1));
+        rugby_sender
+            .send(Symbol::Rugby(random::<usize>() % 10))
+            .unwrap();
+    });
+
+    // burada receiver ile kanala gelen mesajları almaya başlıyoruz.
+    // recv() çağrısı ana thread'i blokluyor.
+    // Bir mesaj geldiği sürece bu while döngüsü çalışmaya devam edecek.
+    while let Ok(incoming_item) = r.recv() {
+        println!(
+            "{}",
+            // println! makrosunda pattern matching kullanıyoruz
+            // gelen öğe Symbol enum sabitinin hangi değerine uyuyorsa
+            // yine sender tarafında üretilen rastgele sayı kadar bir karakteri ekrana bastırıyoruz.
+            match incoming_item {
+                Symbol::PingPong(c) => "🏓".repeat(c + 1),
+                Symbol::Bowling(c) => "🎳".repeat(c + 1),
+                Symbol::Rugby(c) => "🏉".repeat(c + 1),
+            }
+        );
+    }
 }
 
 enum Symbol {
-    Smile(usize),
-    Hearth(usize),
+    PingPong(usize),
+    Bowling(usize),
+    Rugby(usize),
 }
