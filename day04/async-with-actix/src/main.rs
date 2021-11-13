a92827957b629504512967d3c9d46671cf41849c
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-11-14 00:32:07 +0300

    Second actix sample

--- day04/async-with-actix/src/main.rs
+++ day04/async-with-actix/src/main.rs
@@ -1,13 +1,65 @@
-/* reçetede IoT cihazlarındaki sensör verilerinin toplanması ve yazılması ile ilgili bir örnek icra ediliyor */
+/*     
+    Örnekte bir oyun motorunun sıcaklıklarını yakalayan sensör bilgilerini dinleyen bir aktör söz konusu.
+    actix'in bir başka uyarlaması olarak düşünebiliriz. 
+*/
+use actix::prelude::*;
 use rand::Rng;
+use std::thread;
+use std::time::Duration;
 
-// main metodundan sisteme cevap dönebiliriz. Bu sadece Result tipi ile mümkün olabilir.
+// Mesaj olarak tanımladığımız veri yapısı.
+// derive niteliğinde Message kullanarak mesaj olduğunu ifade ettik.
+#[derive(Debug, Message)]
+#[rtype(result = "()")] // Mesajdan geriye birşey döndürmeyeceğimzi için bu şekilde tanımladık.
+struct TempData(pub u64, pub f64);
+
+// Aktörümüz LogWriter.
+struct LogWriter;
+
+impl Actor for LogWriter {
+    type Context = Context<Self>;
+}
+
+// Aktörümüz dinleyici olark TempData mesajını implemente etmeli.
+// Sistemin bu mesajı bilmesi içinse generic Handler trait'ini TempData mesajı için işlemeli.
+impl Handler<TempData> for LogWriter {
+    type Result = ();
+
+    fn handle(&mut self, message: TempData, _: &mut Self::Context) {
+        println!("Anlık motor sıcaklığı bilgileri.\n\t{:?}", message);
+        thread::sleep(Duration::from_millis(1000));
+    }
+}
+
+// system.run çağrısının sonucu result türündendir. main'den sisteme bir çıktı vermiş oluyoruz.
 fn main() -> std::io::Result<()> {
-    // sistemi çalıştırıp event loop'u başlatıyoruz.
-    System::run(|| {
-        println!("Sistemden çıkmak için Ctrl+C...");
-        
-    })
+    // Yeni bir event loop sistemi tanımladık
+    let system = System::new();
+
+    // Aktörün işlemlerini icra ettiği yer. Bu işlemler sırasında sistemi bekletiyoruz
+    system.block_on(async {
+        // Aktör nesnesini oluşturuyoruz.
+        LogWriter::create(|ctx| {
+            // Mesaj göndermek için adresi aldık
+            let address = ctx.address();
+            println!("Context bilgisi : {:?}",ctx);
+            // sembolik bir döngü
+            for i in 0..10 {
+                let event_id = i as u64;
+                let data=read_engine_temperature(); // sensörde motor sıcaklığını okuduk ki bu iş 1 saniye sürüyor. Gecikmeli bir işlemi simüle ettik.
+                address.do_send(TempData(event_id,data)); // aktör adresini kullanarak mesajı yolluyoruz. Artık aktörün handler sisteminde bu mesaj değerlendirilecek.
+            }
+            LogWriter{}
+        });
+        System::current().stop(); // işlemlerin sonlandığı yer olarak düşünebiliriz. Sistemi durduruyoruz. Eğer koymazsak sistem bir programı kapatana kadar açık kalır. Sürekli dinelemeli durumlarda bunu tercih edebiliriz.
+    });
+
+    system.run() 
+
+    // match result {
+    //     Ok(r)=>println!("{:?}",r),
+    //     _ =>println!("Bir hata oluştu")
+    // }
 }
 
 // Motor sıcaklığını ölçen bir sensörü taklit eden mock fonksiyon
@@ -16,8 +68,3 @@ fn read_engine_temperature() -> f64 {
     let heat: f64 = rng.gen_range(-40.0..80.0);
     heat
 }
-
-// Mesaj olarak tanımladığımız veri yapısı.
-// derive niteliğinde Message kullanarak mesaj olduğunu ifade ettik.
-#[derive(Debug, Message)]
-struct TempData(pub u64, pub f64);
