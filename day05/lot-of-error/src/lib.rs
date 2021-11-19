d2a5a0775453a2296387f4e8315434fd863142be
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-11-20 00:03:10 +0300

    some fix and complete

--- day05/lot-of-error/src/lib.rs
+++ day05/lot-of-error/src/lib.rs
@@ -1,12 +1,35 @@
 #[cfg(test)]
 mod tests {
+
+    use super::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn should_errors_work_test() {
+        let pos_err = format!("{}", ErrorWrapper::Device(PosConnectionError(13)));
+        assert_eq!(
+            "13 numaralı cihaz erişilebilir durumda değil. [İletişim Problemi.]",
+            pos_err
+        );
+
+        let opr_err = format!(
+            "{}",
+            ErrorWrapper::Operation(MissingOperationError("SendPhoto"))
+        );
+        assert_eq!(
+            "Çağırılmak istenen 'SendPhoto' operasyonu bulunamadı. [Olmayan Servis Operasyonu.]",
+            opr_err
+        );
+
+        let io_err = format!(
+            "{}",
+            ErrorWrapper::FileSystem(io::Error::from(io::ErrorKind::InvalidData))
+        );
+        assert_eq!("invalid data [invalid data]", io_err);
     }
 }
 
+use std::error::Error; // Error trait'i için gerekir
+use std::fmt; // Display trait'i için gerekir
 use std::io;
 
 /*
@@ -16,31 +39,68 @@ use std::io;
     Error kelimesi ile bitmeleri bir isimlendirme standardıdır.
     Aşağıda üç örnek tanım görülmekte.
 */
-pub struct PosConnectionError(usize);
-pub struct FunctionCallError(usize);
-pub sturct UnexpectedCommError{}
+#[derive(Debug)]
+struct PosConnectionError(usize);
+#[derive(Debug)]
+struct MissingOperationError<'a>(&'a str); // string referans olduğunda lifetime parametresi kullandık
+#[derive(Debug)]
+struct UnexpectedCommError();
 
 // Birden fazla hatanın ele alınacağı durumlarda bunları temsil eden bir enum sabiti kullanmak yararlıdır
-pub enum ErrorWrapper{
+#[derive(Debug)]
+enum ErrorWrapper<'a> {
     Device(PosConnectionError),
-    Operation(MissingOperationError),
+    Operation(MissingOperationError<'a>),
     Unknown(UnexpectedCommError),
-    Io(io::Error)
+    FileSystem(io::Error),
 }
 
 /*
     Standart olarak hatalar bir description fonksiyonu sunarlar.
     Kendi tanımladığımız hatalara Error trait'ini uygulayarak bu davranışı kazandırabiliriz.
 */
-impl Error for ErrorWrapper{
-    // description fonksiyonu hata ile ilgili bilgi döner.
-    fn description(&self)-> &str{
+impl<'a> Error for ErrorWrapper<'a> {
+    // description fonksiyonu hata ile ilgili kısa bilgi döner.
+    fn description(&self) -> &str {
         // match ile ErrorWrapper enum sabitindeki olası tüm durumlar ele alınır.
-        match *self{
-            ErrorWrapper::Device(_)=>"Cihazla iletişim kurulamıyor.",
-            ErrorWrapper::Operation(_)=>"İstenen servis operasyonu bulunamadı.",
-            ErrorWrapper::Io(ref e)=>e.description(), // Io, dosya girdi çıktı hatası ve rust'ın önceden tanımlı yapılarından. Dolayısıyla onun description fonksiyon sonucunu döndürüyor.
-            _ => "Beklenmeye bir hata söz konusu." // Yukarıdaki durumlar haricindeki bir durum oluşması halinde bu kısım çalışır.
+        match *self {
+            ErrorWrapper::Device(_) => "İletişim Problemi.",
+            ErrorWrapper::Operation(_) => "Olmayan Servis Operasyonu.",
+            ErrorWrapper::FileSystem(ref e) => e.description(), // Io, dosya girdi çıktı hatası ve rust'ın önceden tanımlı yapılarından. Dolayısıyla onun description fonksiyon sonucunu döndürüyor.
+            _ => "Bilinmeyen Hata.", // Yukarıdaki durumlar haricindeki bir durum oluşması halinde bu kısım çalışır.
+        }
+    }
+}
+
+/*
+    Hatalara ait detayların sistemin Display davranışını çağıran noktalarda kullanılabilmesi için,
+    ErrorWrapper tipine bu Trait'in de uygulanması ve fmt fonksiyonunun yazılması gerekir.
+*/
+impl<'a> fmt::Display for ErrorWrapper<'a> {
+    /*
+        fmt fonksiyonunun ikinci parametresi metin içeriği formatlamak isteyen Formatter tipindendir.
+        write! makrosu yardımıyla parametre olarak gelen Formatter üstüne gerekli hata bilgileri yazdırılır.
+    */
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            ErrorWrapper::Device(ref e) => write!(
+                f,
+                "{} numaralı cihaz erişilebilir durumda değil. [{}]",
+                e.0,                // PosConnectionErro'un sayısal değeri
+                self.description()  // Kısa tanımı.
+            ),
+            ErrorWrapper::FileSystem(ref e) => write!(f, "{} [{}]", e, self.description()),
+            ErrorWrapper::Operation(ref e) => write!(
+                f,
+                "Çağırılmak istenen '{}' operasyonu bulunamadı. [{}]",
+                e.0,
+                self.description()
+            ),
+            ErrorWrapper::Unknown(_) => write!(
+                f,
+                "İletişim sırasında bilinmeyen bir hata oluştu. [{}]",
+                self.description()
+            ),
         }
     }
 }
