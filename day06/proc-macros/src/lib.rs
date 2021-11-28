c58a019c4007434e3488971c888e2c78d791d53f
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-11-28 22:20:42 +0300

    interesting things.

--- day06/proc-macros/src/lib.rs
+++ day06/proc-macros/src/lib.rs
@@ -1,5 +1,7 @@
 extern crate proc_macro;
-use proc_macro::TokenStream;
+use proc_macro::{Ident, TokenStream};
+use quote::quote;
+use syn::DeriveInput;
 
 /*
     Attribute tabanlı makrolarda proc_macro_attribute niteliği ile imzalanan fonksiyonlar söz konusudur.
@@ -10,7 +12,24 @@ use proc_macro::TokenStream;
 */
 #[proc_macro_attribute]
 pub fn serialize(attributes: TokenStream, input: TokenStream) -> TokenStream {
-    println!("Attributes Stream: \"{}\"", attributes.to_string());
-    println!("Item Stream: \"{}\"", input.to_string());
-    input
+    // Case 1: Olayı anlamaya çalışıyorken.
+
+    // println!("Attributes Stream: \"{}\"", attributes.to_string());
+    // println!("Item Stream: \"{}\"", input.to_string());
+    // input
+
+    // Case 2: Gelen syntax tree'yi yakalayıp identifier bilgisini almak ve farklı bir TokenStream (get_pi fonksiyonu) döndürmek.
+
+    // input'u Syntax Tree'ye çevirmek için syn modülündeki parse komutundan yararlanılıyor.
+    let syntaxtree: DeriveInput = syn::parse(input).unwrap();
+    let name = &syntaxtree.ident; // makronun uygulandığı tipin tanımlayıcısını(adını) ident ile almaktayız.
+    println!("Tipin adı {}", name);
+
+    // Burada quote! makrosundan yararlanarak TokenStream'i değiştirdik.
+    let expanded = quote! {
+        fn get_pi() -> f32 {
+            3.1415
+        }
+    };
+    TokenStream::from(expanded)
 }
--- day06/proc-macros/tests/macrotests.rs
+++ day06/proc-macros/tests/macrotests.rs
@@ -9,9 +9,18 @@ struct Player {
 
 #[test]
 fn should_serialize_works_test() {
-    let _marine = Player {
-        id: 2,
-        name: "Marine Corp".to_owned(),
-        level: 10,
-    };
+
+    // Case 1: Player struct'ı sorunsuz şekilde oluşur ve ilk compile sırasında serialize makrosundaki case 1 çalışır.
+    // Yani argüman ve struct'a ait Syntax Tree terminale basılır.
+    // let _marine = Player {
+    //     id: 2,
+    //     name: "Marine Corp".to_owned(),
+    //     level: 10,
+    // };
+
+    // Case 2: Bu oldukça enteresan oldu.
+    // Ortamda get_pi diye bir fonksiyon yok. Ancak serializable makrosu içinden quote! makrosunu kullanarak derleme aşamasında
+    // bu fonksiyonun koda eklenmesini sağlıyoruz.
+    let pi = get_pi();
+    assert_eq!(pi, 3.1415);
 }
